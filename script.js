129bdfdc4656ba8693806f57a42a06a6673c37ca
taotaoangY <164818408+taotaoangY@users.noreply.github.com>
2024-06-07 17:28:40 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -159,85 +159,8 @@ window.onclick = function(event) {
         }    
     }    
   } 
-  document.getElementById('username').textContent = localStorage.getItem('username');
+ 
 
-let cart = {  
-    items: {},  
-    totalItems: 0,  
-    totalPrice: 0  
-  };  
-    
-  function addToCart(itemId, itemName, price) {  
-    if (!cart.items[itemId]) {  
-      cart.items[itemId] = {  
-        name: itemName,  
-        price: price,  
-        quantity: 1  
-      };  
-    } else {  
-      cart.items[itemId].quantity++;  
-    }  
-    updateCart();  
-  }  
-    
-  function removeFromCart(itemId) {  
-     
-      delete cart.items[itemId];  
-     
-    updateCart();  
-  }  
-    
-  function updateQuantity(itemId, quantity) {  
-    if (cart.items[itemId]) {  
-      cart.items[itemId].quantity = quantity;  
-      updateCart();  
-    }  
-  }  
-    
-  function updateCart() {  
-    let cartHtml = '';  
-    cart.totalItems = 0;  
-    cart.totalPrice = 0;  
-    for (let itemId in cart.items) {  
-      let item = cart.items[itemId];
-      let itemTotalPrice = item.price * item.quantity;  
-      cartHtml += `<div class="cart-item">  
-        <span>${item.name}</span>  
-        <div class="cart-item-quantity-wrapper">  
-          <button onclick="updateQuantity('${itemId}', ${item.quantity - 1})" class="cart-item-quantity-button">-</button>  
-          <input type="number" class="cart-item-quantity-input" value="${item.quantity}" min="1" onchange="updateQuantity('${itemId}', this.value)">  
-          <button onclick="updateQuantity('${itemId}', ${item.quantity + 1})" class="cart-item-quantity-button">+</button>  
-        </div>
-        <span class="cart-item-price">Price:A$${item.price}</span>   
-        <span class="cart-item-price">Total:A$${itemTotalPrice}</span>  
-        <button onclick="removeFromCart('${itemId}')">remove</button>  
-      </div>`;  
-      cart.totalItems += item.quantity;    
-      cart.totalPrice += itemTotalPrice; 
-    }  
-    document.getElementById('cart-items').innerHTML = cartHtml;  
-    document.getElementById('cart-total-items').textContent = cart.totalItems;  
-    document.getElementById('cart-total-price').textContent = 'A$' + cart.totalPrice.toFixed(2);  
-  }  
-    
-  // Initialize shopping cart display (if there are products passed through URL parameters)  
-  window.onload = function() {  
-    updateCart();  
-  }; 
-  function clearCart() {  
-    cart.items = {}; // Clear items from shopping cart  
-    cart.totalItems = 0; // Reset total product quantity  
-    cart.totalPrice = 0; // Reset total price  
-    updateCart(); // Update shopping cart display
-  }
-  function checkout() {  
-    alert(
-      'You bought ' + cart.totalItems +' products'+"\n"+
-      'Your total price is A$ ' + cart.totalPrice
-    );
-    clearCart()  
-};
-// Simple storage of user information
 function registerUser() {  
   var username = document.getElementById('username').value;  
   var password = document.getElementById('password').value;  
@@ -246,7 +169,7 @@ function registerUser() {
   var Grade = document.getElementById('Grade').value;
   var Country = document.getElementById('Country').value;  
 
-  //Here, we simply store user information in localStorage  
+ 
   localStorage.setItem('username', username);  
   localStorage.setItem('password', password);  
   localStorage.setItem('email', email);
@@ -254,23 +177,22 @@ function registerUser() {
   localStorage.setItem('Grade', Grade);
   localStorage.setItem('Countryl', Country);  
 
-  //Jump to login page  
+ 
   window.location.href = 'login.html';  
 }
 
 function setLoginStatus(isLoggedIn) {  
-  // Using LocalStorage to Store Login Status  
+ 
   localStorage.setItem('isLoggedIn', isLoggedIn ? 'true' : 'false');  
 }
 
-// validate logon Form Validation 
+
 function loginUser() {  
   var username = document.getElementById('username').value;  
   var password = document.getElementById('password').value; 
 
-  //Check if there is a corresponding username and password in localStorage  
+  
   if (localStorage.getItem('username') === username && localStorage.getItem('password') === password)  {  
-      // Login successful, redirect to shopping page
       setLoginStatus(true); 
       window.location.href = 'courseware2.html';  
   } else {  
@@ -310,5 +232,5 @@ function setSessionTimeout(minutes) {
        
       window.location.href = 'login.html';  
     }  
-  }, 1000 * 60); 
+  }, 100 * 60); 
 }   
